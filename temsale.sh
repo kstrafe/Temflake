9bf95c2ca29af5402128bda23cc1e20d47a9d70f
Bourgond Aries <macocio@gmail.com>
2016-08-22 05:19:32 +0200

    Show useful info and abort on errors

--- temsale.sh
+++ temsale.sh
@@ -1,6 +1,8 @@
+set -e
 sleeptime=0.07s
 sleep 3s
 while true; do
+	old=$(date +%s%N)
 	xdotool key Return
 	sleep ${sleeptime}
 	xdotool key Down
@@ -21,4 +23,8 @@ while true; do
 	done
 	xdotool key Up
 	sleep ${sleeptime}
+	new=$(date +%s%N)
+	echo $(((new - old) / 100)) total for 8 GOLD
+	echo $((((new - old) / 1000) / 8)) microseconds for 1 GOLD
+	echo $(bc <<< '3600 / 0.'"$(((new - old) / 1000 / 8))") GOLD per hour
 done
